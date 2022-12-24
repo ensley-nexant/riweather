6357dcc516a2ae62e622def1d41e404322afa7ef
ensley-nexant <jensley@resource-innovations.com>
2022-12-24 16:52:33 -0700

    Create test suite structure
    - Declare pytest as a dependency in the "tests" group
    - Create test suite package
    - Write a passing test for the CLI

---
+++ tests/test_cli.py
@@ -0,0 +1,14 @@
+import click.testing
+import pytest
+
+from src.riweather import cli
+
+
+@pytest.fixture
+def runner():
+    return click.testing.CliRunner()
+
+
+def test_main_succeeds(runner):
+    result = runner.invoke(cli.main)
+    assert result.exit_code == 0
