817725bfd517a096b3c2b2e2e7c5d23abc9314e2
ensley-nexant <jensley@resource-innovations.com>
2022-12-26 17:23:56 -0700

    Improve error handling around FTP connection
    - New `NOAAFTPConnectionException` class
    - New exception raised when we can't connect to the FTP server or when we look for a file that doesn't exist
    - Unit tests now cover these cases

--- src/riweather/connection.py
+++ src/riweather/connection.py
@@ -7,6 +7,10 @@ from io import BytesIO
 from riweather import utils
 
 
+class NOAAFTPConnectionException(Exception):
+    pass
+
+
 class NOAAFTPConnection:
     _host = "ftp.ncei.noaa.gov"
 
@@ -14,9 +18,12 @@ class NOAAFTPConnection:
         self.ftp: ftplib.FTP | None = None
 
     def __enter__(self):
-        ftp = ftplib.FTP(host=self._host, timeout=30)
-        ftp.login()
-        self.ftp = ftp
+        try:
+            ftp = ftplib.FTP(host=self._host, timeout=30)
+            ftp.login()
+            self.ftp = ftp
+        except OSError:
+            raise NOAAFTPConnectionException(f"Could not connect to the host: {self._host}.")
         return self
 
     def __exit__(self, *args):
@@ -29,9 +36,12 @@ class NOAAFTPConnection:
         self.ftp = None
 
     def read_file_as_bytes(self, filename: str | os.PathLike) -> typing.IO:
-        stream = BytesIO()
-        self.ftp.retrbinary("RETR {}".format(filename), stream.write)
-        stream.seek(0)
+        try:
+            stream = BytesIO()
+            self.ftp.retrbinary("RETR {}".format(filename), stream.write)
+            stream.seek(0)
+        except ftplib.all_errors as e:
+            raise NOAAFTPConnectionException(e)
 
         if utils.is_compressed(filename):
             return gzip.open(stream, "rb")
